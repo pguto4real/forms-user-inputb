0b14b6d01b0cc450abb3c38ae87f1644f91985af
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-07 04:35:29 -0600

    using form action

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,10 @@
 export default function Signup() {
+  function handleSubmit(formData) {
+    const enteredEmail = formData.get("email");
+    console.log(enteredEmail);
+  }
   return (
-    <form>
+    <form action={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
